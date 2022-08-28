9482419e49d510fe17d37e8d8112574527f911f4
Adarshpreet kaur <adarshpreetkaur@AdarshpeetsMini.Optilink>
2022-08-28 13:53:11 +0530

    Initial project setup1

---
+++ Alert/AlertSelect.swift
@@ -0,0 +1,17 @@
+//
+//  AlertSelect.swift
+//  Alert
+//
+//  Created by Adarshpreet kaur on 27/08/22.
+//
+
+import Foundation
+
+public class AlertSelect {
+    public init() {}
+    
+    public func log(message: String) {
+        print("Log message: ", message)
+    }
+}
+
---
+++ build/Alert.xcframework/ios-arm64_x86_64-simulator/Alert.framework/Headers/Alert.h
@@ -0,0 +1,18 @@
+//
+//  Alert.h
+//  Alert
+//
+//  Created by Adarshpreet kaur on 27/08/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Alert.
+FOUNDATION_EXPORT double AlertVersionNumber;
+
+//! Project version string for Alert.
+FOUNDATION_EXPORT const unsigned char AlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Alert/PublicHeader.h>
+
+
